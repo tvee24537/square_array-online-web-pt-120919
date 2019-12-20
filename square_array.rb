f3a908a6fda6da337065d3c5f7e18917c230f328
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-20 22:09:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,16 @@
 def square_array(numbers)
   # your code here
+<<<<<<< HEAD
   sqNumbers = []
   numbers.each do |e|
     sqNumbers << e ** 2
   end
   return sqNumbers
+=======
+  numbers = []
+  numbers.each do |e|
+    numbers << e ** 2
+  end
+  return numbers
+>>>>>>> e65fbcb495c880043f7bbcaec6a977278a618176
 end
